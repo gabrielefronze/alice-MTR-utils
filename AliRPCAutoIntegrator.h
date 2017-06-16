5c244eaa4250fb79a606e2cea14e798668604abe
Gabriele Fronzé <sucre.91@hotmail.it>
2017-06-16 12:00:58 +0200

    Added routine to check alien token status

--- AliRPCAutoIntegrator.h
+++ AliRPCAutoIntegrator.h
@@ -97,6 +97,18 @@ private:
     Double_t fBigArea[kNPlanes];
     static const Int_t *fColors;//[kNRPC] = {kBlack,kRed,kGreen,kBlue,kYellow,kMagenta,kCyan,kGray,kOrange};
     static const Int_t *fStyles;//[kNPlanes]={20,24,21,25};
+    
+    TString checkTokenBashCommand = "if [[ \"`alien-token-info | grep \"still valid\" `\" != \"\" ]]; then echo \"1\"; else echo \"0\"; fi";
+    Bool_t checkAlienToken()
+    {
+        Bool_t returnValue = kFALSE;
+        FILE * f = popen(checkTokenBashCommand.Data(), "r");
+        char buf[1];
+        size_t read = fread((void *)&buf[0], 1, 1, f);
+        returnValue = (buf[0] == '1');
+        pclose(f);
+        return returnValue;
+    }
 
 public:
 //    TTree *fOCDBTree;
